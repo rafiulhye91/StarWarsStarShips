7bbcf3a9e1b5ffe1431886eced987e6f0f5ea437
Rafiul Hye <rafirupak@gmail.com>
2022-03-11 16:06:48 -0600

    Implemented ViewModel to update livedata from use case
    - Added dependencies for ViewModel and Livedata
    - Created a data class that represents the state of starship data state
    (is it loading, got any error or received the data)
    - Implemented the ViewModel that makes the coroutine calls to update
    livedata calling the use case

---
+++ app/src/main/java/com/example/starwarsstarships/presentation/viewmodel/MainViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.starwarsstarships.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.starwarsstarships.common.Resource
+import com.example.starwarsstarships.domain.usecase.GetStarshipsUsecase
+import com.example.starwarsstarships.presentation.state.StarshipsState
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class MainViewModel(private val getStarshipsUsecase: GetStarshipsUsecase):ViewModel() {
+
+    private val _mStarshipsState: MutableLiveData<StarshipsState>  = MutableLiveData<StarshipsState>()
+    val mStarshipsState:LiveData<StarshipsState> = _mStarshipsState
+
+    fun getStarships(){
+        getStarshipsUsecase().onEach {
+            if(it is Resource.Loading){
+                _mStarshipsState.value = StarshipsState(isLoading = true)
+            }
+            if(it is Resource.Success){
+                _mStarshipsState.value = StarshipsState(starShips = it.data, isLoading = false)
+            }
+            if(it is Resource.Error){
+                _mStarshipsState.value = StarshipsState(error = it.errMessage,isLoading = false)
+            }
+        }.launchIn(viewModelScope)
+    }
+}
